4c331ffcf7d7c4a4f931e0459e76e4ec77d760d2
kylin-dong123 <2360817768@qq.com>
2021-01-26 14:36:18 +0800

    页面搭建完成

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//登录路由配置
+router.get('/login',function(req,res){
+  res.render('login',{})
+})
+//注册路由配置
+router.get('/zhuce',function(req,res){
+  res.render('zhuce',{})
+})
+//详情页路由配置
+router.get('/xiangq',function(req,res){
+  res.render('xiangq',{})
+})
+//write页路由配置
+router.get('/write',function(req,res){
+  res.render('write',{})
+})
 module.exports = router;
